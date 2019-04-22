ee30a387783bc7ce416341365940b1ec9865bda0
Tyeth Gundry <tyethgundry@googlemail.com>
2019-04-22 22:49:41 +0100

    Added test for normal and forced behaviour

--- src/App.test.js
+++ src/App.test.js
@@ -3,17 +3,27 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {mount,shallow} from 'enzyme';
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-//   ReactDOM.unmountComponentAtNode(div);
-// });
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders and hides loading section with default behaviour',()=>{
+  jest.useFakeTimers();
+  const wrapper = mount(<App />);
+
+  jest.runAllTimers();
+  expect(setTimeout.mock.calls.length).toBe(2);
+  wrapper.unmount();
+})
 
 it('renders a loading section with reset timer paused',()=>{
   jest.useFakeTimers();
   const wrapper = mount(<App resetTimerPaused={true} />);
+
   jest.runAllTimers();
+  expect(setTimeout.mock.calls.length).toBe(1);
   expect(wrapper.html()).toContain("loading...");
-  wrapper.debug();
   wrapper.unmount();
 })

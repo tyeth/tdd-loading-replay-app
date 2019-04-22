1aadd21fabcf2e835a3004ab89aad923605b3035
Tyeth Gundry <tyethgundry@googlemail.com>
2019-04-22 16:08:27 +0100

    Added delay to LoadingIndicator logic

--- src/Components/LoadingIndicator/LoadingIndicator.js
+++ src/Components/LoadingIndicator/LoadingIndicator.js
@@ -1,12 +1,30 @@
-import { Component } from 'react'
+import { React,Component} from 'react'
 import { PropTypes } from 'prop-types'
 
-export default class LoadingIndicator extends Component {
+class LoadingIndicator extends Component {
+    static propTypes={
+        isLoading: PropTypes.bool.isRequired
+    }
+
+    state={
+        isPastDelay:false
+    };
+
+    componentDidMount(){
+        this._delayTimer = setTimeout(
+          ()=>  this.setState({isPastDelay:true})
+        , 200);
+    }
+
     render() {
-        if(this.props.isLoading)return null;
+        if(this.props.isLoading){
+            if(!this.state.isPastDelay){
+                return null;
+            }
+            return <div>loading...</div>;
+        }
         return this.props.children;
     };
-}
-LoadingIndicator.propTypes = {
-    isLoading: PropTypes.bool.isRequired
 };
+
+export default LoadingIndicator;
